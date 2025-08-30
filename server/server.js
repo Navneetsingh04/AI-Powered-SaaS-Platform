557c2101d3b1c59bda57fa0a58086e30fb8f4b45
Navneet Singh <navneetsingh1825@gmail.com>
2025-08-30 17:29:01 +0530

    Initialize server with Express, CORS, and Clerk authentication middleware

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
+
+dotenv.config();
+
+// Routes
+import aiRouter from "./routes/ai.routes.js";
+import connectCloudinary from "./config/Cloudinary.js";
+
+const app = express();
+await connectCloudinary()
+
+// Middlewares
+
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.get("/", (req, res) => {
+  res.send("Server is Running");
+});
+
+app.use(requireAuth());
+app.use('/api/ai', aiRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on Port ${PORT}`);
+});
