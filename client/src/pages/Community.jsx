4c793d888db04141e21f79c4117ca9f3e89fba79
Navneet Singh <navneetsingh1825@gmail.com>
2025-08-30 23:40:00 +0530

    Enhance Community component: implement API integration for fetching creations and liking functionality with error handling

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,14 +1,54 @@
 import React, { useEffect, useState } from "react";
 import { useUser } from "@clerk/clerk-react";
-import { dummyPublishedCreationData } from "../assets/assets";
 import { Heart } from "lucide-react";
+import axiosInstace from "../lib/axios.js";
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
 
 const Community = () => {
   const [creations, setCreations] = useState([]);
   const { user } = useUser();
+  const [loading, setLoading] = useState(true);
+  const { getToken } = useAuth();
 
   const fetchCreations = async () => {
-    setCreations(dummyPublishedCreationData);
+    try {
+      const { data } = await axiosInstace.get(
+        "/api/user/get-published-creations",
+        {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        }
+      );
+      if (data.success) {
+        setCreations(data.creation);
+      } else {
+        toast.error(data.message || data.error || "An error occurred");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const imageLikeToggle = async (id) => {
+    try {
+      const { data } = await axiosInstace.post(
+        "/api/user/toggle-like-creations",
+        { id },
+        {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        await fetchCreations();
+      } else {
+        toast.error(data.message || data.error || "An error occurred");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
@@ -17,10 +57,10 @@ const Community = () => {
     }
   }, [user]);
 
-  return (
+  return !loading ? (
     <div className="flex-1 h-full flex flex-col gap-4 p-6">
       Creations
-      <div className="bg-white h-full w-full rounded-xl overflow-y-scroll ">
+      <div className="bg-white h-full w-full rounded-xl overflow-y-scroll">
         {creations.map((creation, index) => (
           <div
             key={index}
@@ -31,16 +71,29 @@ const Community = () => {
               alt=""
               className="w-full h-full object-cover rounded-lg"
             />
-            <div className="absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between p-3 group-hover:bg-gradient-to-b from-transparent to-block/80 text-white rounded-lg">
-              <p className="text-sm hidden group-hover:block">{creation.prompt}</p>
+            <div className="absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between p-3 group-hover:bg-gradient-to-b from-transparent to-black/80 text-white rounded-lg">
+              <p className="text-sm hidden group-hover:block">
+                {creation.prompt}
+              </p>
               <div className="flex gap-1 items-center">
-                <p>{creation.likes.length}</p>
-                <Heart className={`min-w-5 h-5 hover:scale-110 cursor-pointer ${creation.likes.includes(user.id) ? `fill-red-500 text-red-600` : "text-white"}`} />
+                <p>{(creation.likes || []).length}</p>
+                <Heart
+                  onClick={() => imageLikeToggle(creation.id)}
+                  className={`min-w-5 h-5 hover:scale-110 cursor-pointer ${
+                    (creation.likes || []).includes(user.id)
+                      ? "fill-red-500 text-red-600"
+                      : "text-white"
+                  }`}
+                />
               </div>
             </div>
           </div>
         ))}
       </div>
+    </div>
+  ) : (
+    <div className="flex justify-center items-center h-full">
+      <span className="size-10 my-1 rounded-full border-3 border-primary border-t-transparent animate-spin"></span>
     </div>
   );
 };
